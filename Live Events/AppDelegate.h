13b1ac1046b863485ea2292142e7f203b4e69eb9
Alex Medearis <alexmedearis@gmail.com>
2013-06-17 16:11:54 -0500

    first commit

---
+++ Live Events/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Live Events
+//
+//  Created by Alex Medearis on 6/14/13.
+//  Copyright (c) 2013 Bazaarvoice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
